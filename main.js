179549ff8629e6c5f6f98efffc6eb522b9681f2f
fmallari <fc.mallari@gmail.com>
2023-01-28 13:36:04 -0800

    initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+//ES2015 Refactoring
+
+//ES5 Assigning Variables to Object Properties 
+
+var obj = {
+    numbers: {
+      a: 1,
+      b: 2
+    }
+  };
+  
+var a = obj.numbers.a;
+var b = obj.numbers.b;
+
+//Object Destructuring 
+
+//ES5 Array Swap 
+
+var arr = [1, 2];
+var temp = arr[0];
+arr[0] = arr[1];
+arr[1] = temp;
+
+// ES2015 One-Line Array Swap with Destructuring 
+
+
+//raceResults 
+
+
