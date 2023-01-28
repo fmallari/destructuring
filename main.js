2760b5341e344836955122ff683ad7aa9078cb3d
fmallari <fc.mallari@gmail.com>
2023-01-28 13:41:56 -0800

    practice complete

--- main.js
+++ main.js
@@ -14,6 +14,15 @@ var b = obj.numbers.b;
 
 //Object Destructuring 
 
+const obj = {
+    numbers: {
+        a: 1.
+        b: 2
+    }
+};
+
+const {a,b} = obj.numbers
+
 //ES5 Array Swap 
 
 var arr = [1, 2];
@@ -23,7 +32,13 @@ arr[1] = temp;
 
 // ES2015 One-Line Array Swap with Destructuring 
 
+[arr [0], arr [1]] = [arr[1], arr[0]]
 
 //raceResults 
 
+raceResults(['Tom', 'Margaret', 'Allison', 'David', 'Pierre'])
+
+// {first: "Tom", second: "Margaret", third: "Allison", rest: ["David", "Pierre"]}
+
+const raceResults = ([first, second, third, ...rest]) => ({first, second, third, rest})
 
